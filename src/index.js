d511ed81b8074050113161396b0cd6d6f1a22f50
Kasiemobi Eke <89107475+ekekasiemobi@users.noreply.github.com>
2023-03-07 18:58:08 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+
+ReactDom.render(
+  <div>
+    <h1>Hello world</h1>
+    <p>How are you</p> 
+  </div>,
+  document.getElementById("root")
+);
